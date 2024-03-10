e7ae4a71f184519a0ef492683fbe4b5e06d127b3
matvey <matgr2n@gmail.com>
2024-03-10 18:14:06 +0300

    added verbose names to model

--- src/onboarding_forms/models.py
+++ src/onboarding_forms/models.py
@@ -7,6 +7,10 @@ class EstablishmentType(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Тип заведения'
+        verbose_name_plural = 'Типы заведения'
+
 
 class EstablishmentQuestion(models.Model):
     question_text = models.CharField(max_length=255)
@@ -14,6 +18,10 @@ class EstablishmentQuestion(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
 
 
 class Establishment(models.Model):
@@ -29,6 +37,10 @@ class Establishment(models.Model):
     def get_responses(self):
         return ClientResponse.objects.filter(establishment=self)
 
+    class Meta:
+        verbose_name = 'Заведение'
+        verbose_name_plural = 'Заведения'
+
 
 class ClientResponse(models.Model):
     establishment = models.ForeignKey(Establishment, on_delete=models.CASCADE)
@@ -37,3 +49,7 @@ class ClientResponse(models.Model):
 
     def __str__(self):
         return f"{self.establishment} -> {self.question}"
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
